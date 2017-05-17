cb990daa35d5c20ebb1f751cafb809c0f9993b0d
iglewski <iglewski@uqo.ca>
2017-05-16 22:47:28 -0400

    Removed ignored files.

--- src/app/shared/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./pipes/shared-pipes.module"));
-__export(require("./components"));
-__export(require("./modules"));
-__export(require("./guard/auth.guard"));
-//# sourceMappingURL=index.js.map
