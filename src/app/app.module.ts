9045e624ea5faa1c3669d05737975f4040f63ecc
josaphat mayuba <mayubandele@live.fr>
2017-05-29 09:08:14 +0000

    add module all module Authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,21 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppComponent }     from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AuthGuard } from './shared';
+//import { AuthGuard } from './shared';
 
 import { HeroesModule }            from './heroes/heroes.module';
 import { ComposeMessageComponent } from './compose-message.component';
 import { PageNotFoundComponent }   from './not-found.component';
 import { DialogService }           from './dialog.service';
 import { LoginComponent } from './login/index';
+import { HomeComponent } from './home/index';
+import { AuthenticationService, UserService } from './_services/index';
+import { AuthGuard } from './_guards/index';
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/index';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+import { HeaderComponent } from './shared';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: Http) {
@@ -30,7 +38,9 @@ export function HttpLoaderFactory(http: Http) {
     AppComponent,
     LoginComponent,
     ComposeMessageComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -49,8 +59,14 @@ export function HttpLoaderFactory(http: Http) {
     HttpModule,
   ],
     providers: [
-      //AuthGuard
-      DialogService
+      AuthGuard,
+      AuthenticationService,
+      UserService,
+      DialogService,
+      // providers used to create fake backend
+      fakeBackendProvider,
+      MockBackend,
+      BaseRequestOptions
     ],
     bootstrap: [AppComponent]
 })
