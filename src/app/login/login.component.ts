cbbbb4b8f6578a2fc97e6586e474c5d20376f923
josaphat mayuba <mayubandele@live.fr>
2017-05-29 08:38:22 +0000

    use of the authentication service
    The use of the authentication service to log on to the application. It can be used to log in.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
- 
+import { AuthenticationService } from '../_services/index';
 
 @Component({
     moduleId: module.id,
@@ -15,12 +15,23 @@ export class LoginComponent implements OnInit {
 
     constructor(
         private router: Router,
-        ) { }
+        private authenticationService: AuthenticationService) { }
 
     ngOnInit() {
         // reset login status
-         
+        this.authenticationService.logout();
     }
 
-     
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(result => {
+                if (result === true) {
+                    this.router.navigate(['/']);
+                } else {
+                    this.error = 'Username or password is incorrect';
+                    this.loading = false;
+                }
+            });
+    }
 }
