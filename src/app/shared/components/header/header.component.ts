ce4175c3f9281ead21d200212d9cd86d8d54d86e
josaphat mayuba <mayubandele@live.fr>
2017-06-04 20:41:50 -0400

    add collapse

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -4,7 +4,7 @@ import {TranslateService} from '@ngx-translate/core';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
+    styleUrls: ['./header.component.scss']
     
 })
 export class HeaderComponent implements OnInit {
