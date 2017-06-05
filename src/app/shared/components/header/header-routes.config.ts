5f9fbefd3bf4b3e63eaf7669973e03c8acceea0e
josaphat mayuba <mayubandele@live.fr>
2017-06-05 00:30:00 -0400

    edit title login

--- src/app/shared/components/header/header-routes.config.ts
+++ src/app/shared/components/header/header-routes.config.ts
@@ -4,7 +4,7 @@ export const ROUTES: RouteInfo[] = [
   { path: '/corpora', title: 'Corpora', menuType: MenuType.LEFT,icon: '' },
   { path: '/heroes', title: 'Heroes', menuType: MenuType.LEFT ,icon: ''},
   { path: '/admin', title: 'Admin', menuType: MenuType.LEFT,icon: '' },
-  { path: '/login', title: 'login', menuType: MenuType.RIGHT,icon: '' },
+  { path: '/login', title: 'Login', menuType: MenuType.RIGHT,icon: '' },
   { path: 'compose', title: 'Contact', menuType: MenuType.OUTLETRIGHT,icon: '' },
   { path: '/register', title: 'Sign Up', menuType: MenuType.RIGHT ,icon: 'glyphicon glyphicon-user'}
 ];
