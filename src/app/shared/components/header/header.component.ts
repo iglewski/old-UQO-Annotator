ddb33d2a753171ac8cbec4d1943b5631981c07cb
josaphat mayuba <mayubandele@live.fr>
2017-06-12 20:26:47 -0400

    Add toggle for header
    -add bootstrap cdn in src/index.html
    -add header route config
    -add header metadata
    -add routelink in header.component.html
    -edit display in navbar and navbar
    -edit font size for navbar and navbar-brand
    -put the white color in glyphicon

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import { ROUTES } from './header-routes.config';
+import { MenuType } from './header.metadata';
+
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
+    
 })
 export class HeaderComponent implements OnInit {
-
+    public leftMenus: any[];
+    public rightMenus: any[];
+    public rightMenuOutlets: any[];
+    isCollapsed = true;
     constructor(private translate: TranslateService) { }
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.leftMenus = ROUTES.filter(menuItem => menuItem.menuType === MenuType.LEFT);
+        this.rightMenus = ROUTES.filter(menuItem => menuItem.menuType === MenuType.RIGHT);
+        this.rightMenuOutlets = ROUTES.filter(menuItem => menuItem.menuType === MenuType.OUTLETRIGHT);
+    }
+
 
     toggleSidebar() {
         const dom: any = document.querySelector('body');
