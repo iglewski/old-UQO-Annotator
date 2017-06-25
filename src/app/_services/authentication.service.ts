0382af27c327e888c6bfed05242eb1002ff18032
josaphat mayuba <mayubandele@live.fr>
2017-06-25 18:29:14 -0400

    update http post to users/authenticate

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -14,7 +14,7 @@ export class AuthenticationService {
     }
 
     login(username: string, password: string): Observable<boolean> {
-        return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
+        return this.http.post('/users/authenticate', { username: username, password: password })
             .map((response: Response) => {
                 // login successful if there's a jwt token in the response
                 let token = response.json() && response.json().token;
