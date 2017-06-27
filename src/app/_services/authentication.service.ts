ca0701cdd9c6e768b9a586aff8002aa0e11563ce
josaphat mayuba <mayubandele@live.fr>
2017-06-27 15:52:05 -0400

    redirection authetification to app config

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,42 +1,28 @@
 ﻿import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class AuthenticationService {
-    public token: string;
+    constructor(private http: Http) { }
 
-    constructor(private http: Http) {
-        // set token if saved in local storage
-        var currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        this.token = currentUser && currentUser.token;
-    }
-
-    login(username: string, password: string): Observable<boolean> {
+    login(username: string, password: string) {
         return this.http.post('/users/authenticate', { username: username, password: password })
             .map((response: Response) => {
                 // login successful if there's a jwt token in the response
-                let token = response.json() && response.json().token;
-                if (token) {
-                    // set token property
-                    this.token = token;
-
-                    // store username and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify({ username: username, token: token }));
-
-                    // return true to indicate successful login
-                    return true;
-                } else {
-                    // return false to indicate failed login
-                    return false;
+                let user = response.json();
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(user));
                 }
+
+                return user;
             });
     }
 
-    logout(): void {
-        // clear token remove user from local storage to log user out
-        this.token = null;
+    logout() {
+        // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
     }
 }
