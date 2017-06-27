ba3abc18975e5d3572b8443c1e2c784055341fd4
josaphat mayuba <mayubandele@live.fr>
2017-06-27 15:50:56 -0400

    Set id to look like the user model

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,29 +1,29 @@
-import { Injectable } from '@angular/core';
+﻿import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
- 
+
 import { User } from '../_models/index';
- 
+
 @Injectable()
 export class UserService {
     constructor(private http: Http) { }
- 
+
     getAll() {
         return this.http.get('/users').map((response: Response) => response.json());
     }
- 
-    getById(id: string) {
-        return this.http.get('/users/' + id).map((response: Response) => response.json());
+
+    getById(_id: string) {
+        return this.http.get('/users/' + _id).map((response: Response) => response.json());
     }
- 
+
     create(user: User) {
         return this.http.post('/users/register', user);
     }
- 
+
     update(user: User) {
-        return this.http.put('/users/' + user.id, user);
+        return this.http.put('/users/' + user._id, user);
     }
- 
-    delete(id: string) {
-        return this.http.delete('/users/' + id);
+
+    delete(_id: string) {
+        return this.http.delete('/users/' + _id);
     }
 }
