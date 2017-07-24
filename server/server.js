8677419c189d83464cf3f52147bd1607f690b743
josaphat mayuba <mayubandele@live.fr>
2017-07-24 17:07:44 -0400

    upload server

---
+++ server/server.js
@@ -0,0 +1,53 @@
+﻿require('rootpath')();
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
+var fs = require('fs');
+var path = require('path');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use JWT auth to secure the api, the token can be passed in the authorization header or querystring
+app.use(expressJwt({
+    secret: config.secret,
+    getToken: function (req) {
+        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+            return req.headers.authorization.split(' ')[1];
+        } else if (req.query && req.query.token) {
+            return req.query.token;
+        }
+        return null;
+    }
+}).unless({ path: ['/users/authenticate', '/users/register'] }));
+
+// routes
+app.use('/users', require('./controllers/users.controller'));
+
+// start server
+var port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+var server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
+// String -> [String]
+'use strict';
+function getFiles(dir, fileList){
+    fileList = fileList || [];
+
+    var files = fs.readdirSync(dir);
+    for(var i in files){
+        if (!files.hasOwnProperty(i)) continue;
+        var name = dir+'/'+files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, fileList);
+        } else {
+            fileList.push(name);
+        }
+    }
+    return fileList;
+}
+console.log(getFiles('data/pico1'));
