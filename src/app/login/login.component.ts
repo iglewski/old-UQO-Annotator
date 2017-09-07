0530aa79a99daa6954c75970859fd953b41450d4
josaphat mayuba <mayubandele@live.fr>
2017-09-07 19:53:16 -0400

    Fix the indentation.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,42 +1,47 @@
-﻿import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { AlertService, AuthenticationService } from '../_services/index';
+import { HeaderComponent } from '../shared/components/header/header.component';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'login.component.html'
+  moduleId: module.id,
+  templateUrl: 'login.component.html'
 })
 
 export class LoginComponent implements OnInit {
-    model: any = {};
-    loading = false;
-    returnUrl: string;
+  model: any = {};
+  loading = false;
+  returnUrl: string;
 
-    constructor(
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService,
-        private alertService: AlertService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService) {
+  }
 
-    ngOnInit() {
-        // reset login status
-        this.authenticationService.logout();
+  ngOnInit() {
+    // reset login status
+    this.authenticationService.logout();
 
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    }
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
 
-    login() {
-        this.loading = true;
-        this.authenticationService.login(this.model.username, this.model.password)
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
+  login() {
+    this.loading = true;
+    this.authenticationService.login(this.model.username, this.model.password)
+      .subscribe(
+        data => {
+          HeaderComponent.isUserLoggedIn = true;
+          HeaderComponent.updateUserStatus.next(true);
+          this.router.navigateByUrl('/');
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+      });
+  }
+
 }
