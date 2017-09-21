072385cea6b489486b2bbf46f91f6591f249f0dd
josaphat mayuba <mayubandele@live.fr>
2017-09-21 05:06:43 -0400

    create view and component for the project

---
+++ src/app/project/index.ts
@@ -0,0 +1,1 @@
+﻿export * from './project.component';
---
+++ src/app/project/project.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'project',
+  templateUrl: 'project.component.html'
+})
+
+export class ProjectComponent implements OnInit {
+ 
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    
+
+    
+  }
+
+  
+}
