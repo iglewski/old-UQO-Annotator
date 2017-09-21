8933a208a1597994548d051f04b6d7badfa9e1c7
josaphat mayuba <mayubandele@live.fr>
2017-09-20 22:40:57 -0400

    add commentary

--- src/app/_services/fs.service.ts
+++ src/app/_services/fs.service.ts
@@ -5,11 +5,11 @@ import { Http, Headers, RequestOptions, Response } from '@angular/http';
 @Injectable()
 export class FsService {
   constructor(private http: Http) { }
-
+//this is service to get list files a Corpus. this function requests '/fs/getCorpus
   getCorpus() {
     return this.http.get('/fs/getCorpus').map((response: Response) => response.json());
   }
-
+//this is service to get list files a Corpus. this function requests "/ fs / FilesCorpus / '
    getFilesCorpus(Corpus: string) {
       console.log("service "+ Corpus);
       return this.http.get('/fs/FilesCorpus/' + Corpus).map((response: Response) => response.json());;
