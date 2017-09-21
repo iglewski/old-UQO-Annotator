0a10e5c45996d55e2ceca83bc66cd3daf48804fc
josaphat mayuba <mayubandele@live.fr>
2017-09-20 22:34:07 -0400

    Create a service for a request to get the corpus list and file list of a corpus
    - create service fs (file systeme)
    -create request http to get the corpus list
    -create request http to get the files list of the corpus

---
+++ src/app/_services/fs.service.ts
@@ -0,0 +1,17 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+
+@Injectable()
+export class FsService {
+  constructor(private http: Http) { }
+
+  getCorpus() {
+    return this.http.get('/fs/getCorpus').map((response: Response) => response.json());
+  }
+
+   getFilesCorpus(Corpus: string) {
+      console.log("service "+ Corpus);
+      return this.http.get('/fs/FilesCorpus/' + Corpus).map((response: Response) => response.json());;
+  }
+}
