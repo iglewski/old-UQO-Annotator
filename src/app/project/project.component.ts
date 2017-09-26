7ef94b8400b6969a9ce37e686a47c2665ae0cb85
josaphat mayuba <mayubandele@live.fr>
2017-09-26 16:08:29 -0400

    get content from the text for the project

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-
+import { articlesService } from '../_services/index';
 
 @Component({
   moduleId: module.id,
@@ -9,16 +9,24 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 
 export class ProjectComponent implements OnInit {
- 
+ 	
+ 	project: string;
+ 	text: string;
 
-  constructor() {
+  constructor(private ArticlesService: articlesService) {
+  this.project = JSON.parse(localStorage.getItem('linkProject'));
   }
 
   ngOnInit() {
-    
-
+   this.linkStockage();
+  }
+//get content from the text for the project department
+  private linkStockage(){
+  	if(this.project){
+  	console.log(this.project);
+  	this.ArticlesService.getText(this.project).subscribe(text => { this.text = text; });;
+  	} //else the project is not available
     
   }
-
   
 }
