14b69b6bec0141b6e9406a9c5ba1824bc4570beb
josaphat mayuba <mayubandele@live.fr>
2017-09-26 16:13:56 -0400

    this is service to get list files a Corpus.

---
+++ src/app/_services/articles.service.ts
@@ -0,0 +1,14 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+
+@Injectable()
+export class articlesService {
+  constructor(private http: Http) { }
+
+//this is service to get list files a Corpus. this function requests "/ fs / FilesCorpus / '
+   getText(_link: string) {
+      console.log("service UIMA FILE client"+ _link);
+      return this.http.get('/articles/project/' + _link).map((response: Response) => response.text());;
+  }
+}
